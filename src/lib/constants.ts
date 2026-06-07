f1eca71c50d515d87540213b1e42db87716193e5
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-06-07 22:41:31 +0700

    Delete src directory

--- src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const CONTACT_EMAIL_RECEIVER = "alexander@wright-dev.io";
-export const RESUME_DOWNLOAD_URL = "/CV.pdf";
-export const WEBSITE_BASE_URL = "https://wright-dev.io";
-export const ANIMATION_TRANSITION = { type: "spring", stiffness: 100, damping: 15 };
